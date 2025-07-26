8aea68d6d6a79799c94ac0e593344cf30f31c821
libknt <101494707+libknt@users.noreply.github.com>
2025-07-26 17:34:25 +0900

    add

--- sandbox/strcpy/ft_strcpy.s
+++ sandbox/strcpy/ft_strcpy.s
@@ -4,13 +4,12 @@ global ft_strcpy
 ft_strcpy:
     push rbp
     mov rbp, rsp
-
     mov rax, rdi
 
     cmp rdi, 0
     je .end_func
 
-    cmp rsi., 0
+    cmp rsi, 0
     je .end_func
 
 .loop_start:
@@ -19,8 +18,8 @@ ft_strcpy:
     inc rsi
     inc rdi
 
-    cmp rdi 0
-    jnz loop_start
+    cmp rdi, 0
+    jnz .loop_start
 
     je .end_func
 
