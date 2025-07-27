7dd00be87435e4f0bf59410a399c723202726eca
libknt <101494707+libknt@users.noreply.github.com>
2025-07-27 15:08:00 +0900

    fix: enhance ft_read function with improved error handling and syscall logic

--- sandbox/read/ft_read.s
+++ sandbox/read/ft_read.s
@@ -12,7 +12,40 @@ extern __errno_location
     push rbp
     mov rbp, rsp
 
+%ifdef MACOS
+    mov rax, 0x2000003
+%else
+    mov rax, 0
+%endif
 
+    syscall
+    
+    cmp rax, 0
+    jl .error
+
+    jc .error
+
+    jmp .success
+
+.success:
+    mov rax, rdx
+
+    jmp .end_func
+
+.error:
+    neg rax
+
+    mov rdi, rax
+
+%ifdef MACOS
+    call __error
+%else
+    call __errno_location
+%endif
+
+    mov [rax], dword rdi
+
+    mov rax, -1
 
 .end_func:
     pop rbp
