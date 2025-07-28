2fa6053e7c72f4ef4da795e397779c354ab548dc
libknt <101494707+libknt@users.noreply.github.com>
2025-07-28 19:15:44 +0900

    fix: ft_write関数においてmacOS用のエラーチェックとスタックフレームの設定を追加し、Linuxとのコードの切り分けを改善

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -10,48 +10,23 @@ section .text
 global _ft_write
 _ft_write:
 extern ___error
-%else
-global ft_write
-ft_write:
-extern __errno_location
-%endif
     ; スタックフレームの設定
     push rbp                    ; 現在のベースポインタを保存
     mov rbp, rsp               ; 新しいスタックフレームを設定
 
     ; システムコール番号の設定
-%ifdef MACOS
     mov rax, 0x2000004         ; macOS の write システムコール番号
-%else
-    mov rax, 1                 ; Linux の write システムコール番号
-%endif
 
     ; write システムコールを実行
     syscall                    ; システムコール実行 (fd=rdi, buf=rsi, count=rdx)
-    jc .error                  ; Jump if Carry flag set
-
-    ; エラーチェック
-    cmp rax, 0                 ; 戻り値が負の値かどうか確認
-    jl .error                  ; Jump if Less than zero
-    
-
-    jmp .success               ; Jump to success
-
-.success:
-    jmp .end_func              ; Jump to end_func
+    jnc .end_func              ; Jump if not Carry flag set
 
 .error:
     ; エラー処理
-    neg rax                    ; Negate rax
-
-    mov rdi, rax               ; エラーコードを引数として設定
+    mov edi, eax               ; errno（positive）
 
     ; errno の設定
-%ifdef MACOS
     call ___error              ; __error() を呼び出し
-%else
-    call __errno_location      ; __errno_location() を呼び出し
-%endif
 
     mov [rax], edi             ; errno にエラーコードを設定
 
@@ -61,3 +36,39 @@ extern __errno_location
     ; スタックフレームの復元
     pop rbp                    ; 元のベースポインタを復元
     ret                        ; 呼び出し元に戻る (戻り値はraxに格納済み)
+
+%else
+global ft_write
+ft_write:
+extern __errno_location
+    ; スタックフレームの設定
+    push rbp                    ; 現在のベースポインタを保存
+    mov rbp, rsp               ; 新しいスタックフレームを設定
+
+    ; システムコール番号の設定
+    mov rax, 1                 ; Linux の write システムコール番号
+
+    ; write システムコールを実行
+    syscall                    ; システムコール実行 (fd=rdi, buf=rsi, count=rdx)
+    cmp rax, 0                 ; 戻り値が負の値かどうか確認
+    jl .error                  ; Jump if Less than zero
+
+.error:
+    ; エラー処理
+    neg rax                    ; Negate rax
+
+    mov edi, eax               ; エラーコードを引数として設定
+
+    ; errno の設定
+    call __errno_location      ; __errno_location() を呼び出し
+
+    mov [rax], edi             ; errno にエラーコードを設定
+
+    mov rax, -1                ; 戻り値 = -1 (エラー)
+
+.end_func:
+    ; スタックフレームの復元
+    pop rbp                    ; 元のベースポインタを復元
+    ret                        ; 呼び出し元に戻る (戻り値はraxに格納済み)
+
+%endif
